69973d00d10f5df3b3b5bb11a5dc230eadbabba4
Zete Lui <mas@k.ed>
2013-02-03 12:30:02 +0800

    add test for prefix iteration

--- test/triez_test.rb
+++ test/triez_test.rb
@@ -23,7 +23,26 @@ class TriezTest < Test::Unit::TestCase
     assert_equal v2, t['万塘路']
 
     a = []
-    t._internal_search '', 0, -> k, v { a << [k,v] }
+    t._internal_search '', nil, -> k, v { a << [k, v] }
     assert_equal [['万塘路', v2]], a
+
+    a = []
+    t['马当路'] = 3
+    t._internal_search '万塘', nil, -> k, v { a << [k, v] }
+    assert_equal [['路', v2]], a
+  end
+
+  def test_iter
+    t = Triez.hat
+    ('A'..'z').each do |a|
+      # 10k chars to ensure burst
+      ('一'..'百').each do |b|
+        t[a + b] = 0
+      end
+    end
+
+    a = []
+    t._internal_search 'a', nil, -> k, v { a << k }
+    assert_equal ('一'..'百').to_a, a.sort
   end
 end
