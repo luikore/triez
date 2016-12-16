109934268cc5c8cacec118b2ea9553a8f738ed00
Zete Lui <mas@k.ed>
2016-12-16 13:56:22 +0800

    Fix sudo gem install in macOS Sierra #6

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -8,25 +8,9 @@ create_makefile 'triez'
 # respect header changes
 headers = Dir.glob('*.{hpp,h}').join ' '
 File.open 'Makefile', 'a' do |f|
-  f.puts "\n$(OBJS): #{headers}"
-end
-
-# build vendor lib
-def sh *xs
-  puts xs.join(' ')
-  system *xs
-end
-
-require "fileutils"
-include FileUtils
-build_dir = File.dirname(__FILE__) + '/build'
-mkdir_p build_dir
-cd build_dir
-unless File.exist?('libtries.a')
-  cc = ENV['CC'] || RbConfig::CONFIG['CC']
-  cc = [cc, '-O3', '-std=c99', '-Wall', '-pedantic', '-fPIC', '-c']
-  ar = RbConfig::CONFIG['AR']
-  ar = 'ar' unless File.exist?(ar)
-  sh *cc, '-I..', *Dir.glob("../hat-trie/*.c")
-  sh ar, '-r', 'libtries.a', *Dir.glob("*.o")
+  f.puts
+  f.puts "$(OBJS): #{headers}"
+  f.puts "$(DLLIB): build/libtries.a"
+  f.puts "build/libtries.a:"
+  f.puts "\tmkdir -p build && cd build && $(CC) -O3 -std=c99 -Wall -pedantic -fPIC -c -I.. ../hat-trie/*.c && $(AR) -r libtries.a *.o"
 end
--- lib/triez.rb
+++ lib/triez.rb
@@ -1,7 +1,7 @@
 require_relative "../ext/triez"
 
 class Triez
-  VERSION = '1.0.4'
+  VERSION = '1.0.5'
 
   private :_internal_set_type
   private :_internal_search
